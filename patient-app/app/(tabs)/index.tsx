ac6a223c239fc6f7bef88a7a0bf0f974a8125ad4
nezz0746 <nezz0746@gmail.com>
2023-07-22 01:39:36 +0200

    feat: test wallet connect modal

--- patient-app/app/(tabs)/index.tsx
+++ patient-app/app/(tabs)/index.tsx
@@ -1,13 +1,25 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from "react-native";
+import { useWalletConnectModal } from "@walletconnect/modal-react-native";
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import EditScreenInfo from "../../components/EditScreenInfo";
+import { Text, View } from "../../components/Themed";
 
 export default function TabOneScreen() {
+  const { open } = useWalletConnectModal();
   return (
     <View style={styles.container}>
+      <Button
+        title="Hello"
+        onPress={() => {
+          open();
+        }}
+      />
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
@@ -16,16 +28,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
--- patient-app/app/_layout.tsx
+++ patient-app/app/_layout.tsx
@@ -1,23 +1,31 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import "../expo-crypto-shim.js";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { WalletConnectModal } from "@walletconnect/modal-react-native";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
-} from 'expo-router';
+} from "expo-router";
 
 export const unstable_settings = {
   // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+  initialRouteName: "(tabs)",
 };
+
+const projectId = "e79e4d2acccc4c290a89adceded0ac9f";
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
   });
 
@@ -31,6 +39,19 @@ export default function RootLayout() {
       {/* Keep the splash screen open until the assets have loaded. In the future, we should just support async font loading with a native version of font-display. */}
       {!loaded && <SplashScreen />}
       {loaded && <RootLayoutNav />}
+      <WalletConnectModal
+        projectId={projectId}
+        providerMetadata={{
+          name: "YOUR_PROJECT_NAME",
+          description: "YOUR_PROJECT_DESCRIPTION",
+          url: "https://your-project-website.com/",
+          icons: ["https://your-project-logo.com/"],
+          redirect: {
+            native: "YOUR_APP_SCHEME://",
+            universal: "YOUR_APP_UNIVERSAL_LINK.com",
+          },
+        }}
+      />
     </>
   );
 }
@@ -40,10 +61,10 @@ function RootLayoutNav() {
 
   return (
     <>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+          <Stack.Screen name="modal" options={{ presentation: "modal" }} />
         </Stack>
       </ThemeProvider>
     </>
