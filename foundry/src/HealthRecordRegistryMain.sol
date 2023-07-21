9d09acde44a44d4b349c2679ca57c68d750fe7a2
Mattis Deisen <mattis@deisen.net>
2023-07-22 00:57:01 +0200

    Finish foundry setup

--- foundry/src/HealthRecordRegistryMain.sol
+++ foundry/src/HealthRecordRegistryMain.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.19;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract HealthRecordRegistryMain is Ownable {
     constructor() {}
