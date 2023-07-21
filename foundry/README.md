0449388021662b6cefa45da5d9b158505541d7d6
Mattis Deisen <mattis@deisen.net>
2023-07-22 00:22:01 +0200

    WIP: Init foundry

--- foundry/README.md
+++ foundry/README.md
@@ -1,6 +1,6 @@
 # Foundry Template [![Open in Gitpod][gitpod-badge]][gitpod] [![Github Actions][gha-badge]][gha] [![Foundry][foundry-badge]][foundry] [![License: MIT][license-badge]][license]
 
-[gitpod]: https://gitpod.io/#https://github.com/PaulRBerg/foundry-template
+[gitpod]: https://gitpod.io/#https://github.com/PaulRBerg/foundrforge buildy-template
 [gitpod-badge]: https://img.shields.io/badge/Gitpod-Open%20in%20Gitpod-FFB45B?logo=gitpod
 [gha]: https://github.com/PaulRBerg/foundry-template/actions
 [gha-badge]: https://github.com/PaulRBerg/foundry-template/actions/workflows/ci.yml/badge.svg
---
+++ foundry/src/HealthRecordRegistryMain.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.8.19;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract HealthRecordRegistryMain is Ownable {
+    constructor() {}
+
+    function addRecordType() public onlyOwner returns (bytes32) {
+        // 
+    }
+
+    function issueRecord() public returns (bytes32) {
+    
+    }
+
+    function _generateRecordId(
+        address _issuer,
+        uint32 _recordId,
+        uint64 _value
+    ) private pure returns (uint256) {
+        return uint160(_issuer) << 96 | uint96(_recordId) << 64 | uint64(_value);
+    }
+}
