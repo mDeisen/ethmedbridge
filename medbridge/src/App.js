2bbf68a824537521a1f0cc33194d48c8718634b5
Daniel Mueller <daniel.mueller@seba.swiss>
2023-07-22 10:46:05 +0200

    adding sismo button

--- medbridge/src/App.js
+++ medbridge/src/App.js
@@ -1,24 +1,66 @@
 import './App.css';
 import OverlayButton from './components/OverlayButton'
 import Sparkline from './components/Sparkline';
+import { SismoConnectButton, AuthType,  SismoConnectConfig, SismoConnectResponse } from "@sismo-core/sismo-connect-react";
 
 function App() {
 
   const sparklineData = [4, 4, 3.7, 4, 4, 4.5, 6, 6.4, 3, 5, 4.4];
+  const config: SismoConnectConfig = {
+    appId: "0xf4977993e52606cfd67b7a1cde717069", 
+  }
 
 
   return (
+
+    
     <div className="App">
       <header className="App-header">
+      
+      <div className="absolute top-0 right-0 p-8">
+<SismoConnectButton 
+// You can also create several auth and claim requests 
+// in the same button
+    config={config}
+    // request multiple proofs of account ownership 
+    // (here Vault ownership and Twitter account ownership)
+    auths={[
+        {authType: AuthType.VAULT},
+        {authType: AuthType.TWITTER},
+    ]}
+    // request multiple proofs of group membership 
+    // (here the groups with id 0x42c768bb8ae79e4c5c05d3b51a4ec74a and 0x8b64c959a715c6b10aa8372100071ca7)
+    claims={[
+        {groupId: "0x42c768bb8ae79e4c5c05d3b51a4ec74a"},
+        {groupId: "0x8b64c959a715c6b10aa8372100071ca7"}
+    ]}
+    signature={{message: "Your message"}}
+    onResponse={async (response: SismoConnectResponse) => {
+	//Send the response to your server to verify it
+	//thanks to the @sismo-core/sismo-connect-server package
+    }}
+    onResponseBytes={async (bytes: string) => {
+        //Send the response to your contract to verify it
+        //thanks to the @sismo-core/sismo-connect-solidity package
+    }}
+/> 
+      
+    </div>
+
+
+
+
+
+
         <h1 className="text-3xl font-bold underline">MedConnect - Patient Dashboard</h1>
         <br></br>
         <h1>Click this button to create QR code to receive Medical Information (ERC6551)</h1>
       <OverlayButton />
       </header>
 
-      <div>
-      
-    </div>
+
+
+    
 
     <main className="container mx-auto mt-8 p-4">
   <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
